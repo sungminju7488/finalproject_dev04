837dca455f43cd08e0899e7c5029eef6475fadef
sungminju7488 <ssmj94@hanmail.net>
2021-05-29 14:13:12 +0900

    member/join기능구현 ->MemberController.java ->MemberService.java

--- backend/src/main/java/com/example/server/member/MemberController.java
+++ backend/src/main/java/com/example/server/member/MemberController.java
@@ -1,5 +1,35 @@
 package com.example.server.member;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
 
+	MemberService memberService;
+	
+	@PostMapping("/member/join")
+	public void join(MemberVO memberVO) {
+		memberService.join(memberVO);		
+	}
+//	@PostMapping("/member/joinBakery")
+//	public void joinBakery(MemberVO memberVO) {
+//		memberService.joinBakery(memberVO);		
+//	}
+//	
+//	@PostMapping("/member/myPage")
+//	public MemberVO myPage(MemberVO memberVO) {
+//		return memberService.myPage(memberVO).get();
+//	}
+//	
+//	@PostMapping("/member/changeMyInfo")
+//	public void update(MemberVO memberVO) {
+//		memberService.update(memberVO);
+//	}
+//	@PostMapping("/member/quit")
+//	public MemberVO quit(MemberVO memberVO) {
+//		
+//		return memberService.delete(memberVO).get();
+//	}
+	
 }
--- backend/src/main/java/com/example/server/member/MemberService.java
+++ backend/src/main/java/com/example/server/member/MemberService.java
@@ -1,5 +1,28 @@
 package com.example.server.member;
 
-public class MemberService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class MemberService {
+	@Autowired
+	MemberRepository mr;
+	
+	public void join(MemberVO memberVO) {
+		mr.save(memberVO);
+	}
+//	public Optional<MemberVO> myPage(MemberVO memberVO){
+//		return mr.findById(memberVO.getMemberId());
+//	}
+//	
+//	public void update(MemberVO memberVO) {
+//		mr.save(memberVO);
+//	}
+//	public Optional<MemberVO> delete(MemberVO memberVO){
+//		return mr.deleteByIdAndPassword(memberVO.getMemberId(), memberVO.getPassword());
+//	}
 }
