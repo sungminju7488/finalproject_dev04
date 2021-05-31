ea6f889b2ac2bf3be4dc16126e4013cbfc42b0ff
signatureh <signature-h@hanmail.net>
2021-05-31 13:20:29 +0900

    백엔드 프로젝트 재생성

---
+++ backend/src/main/java/com/dev4/sunbbang/member/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.dev4.sunbbang.member;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev4.sunbbang.model.MemberVO;
+
+public interface MemberRepository extends JpaRepository<MemberVO, Integer>{
+
+//	Optional<MemberVO> deleteByMemberIdAndPassword(String memberId, String password);
+//	Optional<MemberVO> findByMemberIdAndPassword(String memberId, String password);
+//	Optional<MemberVO> findByPhoneNumberAndEMail(String phoneNumber, String eMail);
+//	Optional<MemberVO> findByMemberId(String memberId);
+
+}
