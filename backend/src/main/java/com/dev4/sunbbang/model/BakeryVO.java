484b2523c87d34a675cf8f579ef5b989e1ad7669
SEOKJY\sky-s <jiyunseok45@gmail.com>
2021-06-01 10:26:27 +0900

    관리자 페이지 BakeryVO.java

--- backend/src/main/java/com/dev4/sunbbang/model/BakeryVO.java
+++ backend/src/main/java/com/dev4/sunbbang/model/BakeryVO.java
@@ -1,53 +1,46 @@
-package com.example.server.model;
+package com.dev4.sunbbang.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
-@SequenceGenerator(name="memberSeq",sequenceName="no_seq",initialValue = 1 , allocationSize = 1)
+@Data
 @Entity
 @Table(name="BAKERY")
 public class BakeryVO {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "no_seq") 
 	@Column(name="COPREGNUM",nullable=false,length=10)
-	private String copregnum;
+	private String copRegNum;
 	@Column(name="MANAGER",nullable=false,length=50)
 	private String manager;
 	@Column(name="STORENAME",nullable=false,length=50)
-	private String storename;
+	private String storeName;
 	@Column(name="STOREADDRESS1",nullable=false,length=100)
-	private String storeaddress1;
+	private String storeAddress1;
 	@Column(name="STOREADDRESS2",nullable=false,length=100)
-	private String storeaddress2;
+	private String storeAddress2;
 	@Column(name="STORECONTACT",nullable=false,length=11)
-	private String storecontact;
+	private String storeContact;
 	@Column(name="BAKERYPATH",nullable=false,length=255)
-	private String phoneNumber;
+	private String bakeryPath;
 	@Column(name="BUSINESSHOURL",nullable=false,length=20)
-	private String businesshour;
+	private String businessHour;
 	@Column(name="HOLIDAY",length=100)
 	private String holiday;
 	@Column(name="SPECIALHOLIDAY",length=100)
-	private String specialholiday;
+	private String specialHoliday;
 	@Column(name="EATABLE",nullable=false,length=1)
-	private String eayable;
+	private String eatable;
 	@Column(name="LATITUDE",nullable=false,length=10)
 	private String latitude;
 	@Column(name="LONGITUDE",nullable=false,length=10)
 	private String longitude;
 	@Column(name="BOARDSET",nullable=false,length=1)
-	private String boardset;
+	private String boardSet;
 	@Column(name="MEMBERSEQ",nullable=false,length=8)
-	private String memberseq;
+	private String memberSeq;
 	
 	
 	
