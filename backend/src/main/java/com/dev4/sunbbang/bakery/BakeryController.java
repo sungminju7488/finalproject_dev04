dce4db55ce14e23007304e992f41fac262d9b6a4
SEOKJY\sky-s <jiyunseok45@gmail.com>
2021-06-01 15:19:10 +0900

    bakery Controller 수정

--- backend/src/main/java/com/dev4/sunbbang/bakery/BakeryController.java
+++ backend/src/main/java/com/dev4/sunbbang/bakery/BakeryController.java
@@ -2,12 +2,14 @@ package com.dev4.sunbbang.bakery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.dev4.sunbbang.model.BakeryVO;
+import com.dev4.sunbbang.model.FoodVO;
 import com.dev4.sunbbang.model.MemberVO;
 import com.google.gson.Gson;
 
@@ -17,7 +19,7 @@ public class BakeryController {
 
 	@Autowired
 	BakeryService bakeryService;
-	
+
 	@Autowired
 	Gson gson;
 
@@ -25,14 +27,37 @@ public class BakeryController {
 	public void joinBakery(@RequestBody MemberVO memberVO, BakeryVO bakeryVO) {
 		bakeryService.joinBakery(memberVO, bakeryVO);
 	}
-	
+
 	@RequestMapping("/bakery/myShop")
 	public Object myShop(@RequestBody BakeryVO bakeryVO) {
 		return gson.toJson(bakeryService.myShop(bakeryVO));
 	}
 
-	public void changeBakery(@RequestBody BakeryVO bakeryVO) {
+	@RequestMapping("/bakery/changeBakery")
+	public void changeBakery(@RequestBody BakeryVO bakeryVO, MultipartFile image) {
 		bakeryService.myShop(bakeryVO);
 
 	}
+
+	@RequestMapping("/bakery/menuList")
+	public Object menuList(@RequestBody FoodVO foodVO) {
+		return gson.toJson(bakeryService.menuList(foodVO));
+
+	}
+	
+	@RequestMapping("/bakery/addMenu")
+	public void addMenu(@RequestBody FoodVO foodVO) {
+		bakeryService.addMenu(foodVO);
+	}
+	
+	@RequestMapping("/bakery/modifyMenu")
+	public void modifyMenu(@RequestBody FoodVO foodVO) {
+		bakeryService.modifyMenu(foodVO);
+	}
+	
+	@DeleteMapping("/bakery/deleteMenu")
+	public Object deleteMenu(@RequestBody FoodVO foodVO) {
+		return gson.toJson(bakeryService.deleteMenu(foodVO));
+	}
+	
 }
