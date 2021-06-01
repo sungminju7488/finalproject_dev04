a112b641f51fbf35c6b13a437e2c3f5f1593fe76
SEOKJY\sky-s <jiyunseok45@gmail.com>
2021-06-01 15:23:09 +0900

    메뉴기능 땜에 FoodRepository 생성

---
+++ backend/src/main/java/com/dev4/sunbbang/bakery/FoodRepository.java
@@ -0,0 +1,18 @@
+package com.dev4.sunbbang.bakery;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev4.sunbbang.model.BakeryVO;
+import com.dev4.sunbbang.model.FoodVO;
+import com.dev4.sunbbang.model.MemberVO;
+
+public interface FoodRepository extends JpaRepository<FoodVO, Integer>{
+	
+	
+
+	public Optional<BakeryVO> findByBakerySeq(String bakerySeq);
+
+
+}
