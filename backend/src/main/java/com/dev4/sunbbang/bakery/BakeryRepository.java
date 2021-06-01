f54a1ef7ab8bd32a9f7c3dc1815c2a7f1c1e1b4d
SEOKJY\sky-s <jiyunseok45@gmail.com>
2021-06-01 10:23:19 +0900

    관리자 페이지 BakeryRepository.java

---
+++ backend/src/main/java/com/dev4/sunbbang/bakery/BakeryRepository.java
@@ -0,0 +1,11 @@
+package com.dev4.sunbbang.bakery;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev4.sunbbang.model.BakeryVO;
+
+public interface BakeryRepository extends JpaRepository<BakeryVO, Integer>{
+	public Optional<BakeryVO> findByMemberSeq(String memberSeq);
+}
