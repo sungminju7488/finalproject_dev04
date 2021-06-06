4c8559afab6ad6782f5d85500d5136b0ffe637be
llsee01 <llsee01@naver.com>
2021-06-07 03:08:44 +0900

    로그인페이지 view, main 페이지 carousel 추가

--- frontend/src/component/Main.js
+++ frontend/src/component/Main.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
+import Carousel from 'react-bootstrap/Carousel';
 import {
   Nav,
   Form,
@@ -7,12 +8,14 @@ import {
   Button,
   NavbarBrand,
   NavLink,
+  
 } from "react-bootstrap";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import "../css/Navbar.css";
+import "../css/carousel.css";
 
-function navbar() {
+function main() {
   return (
     <body>
       <nav className="navbar navbar-expand-lg navbar-light bg-#f0e8d9">
@@ -69,8 +72,49 @@ function navbar() {
           </ul>
         </div>
       </nav>
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item item1 active">
+            <div className="helper"></div><div className="intro">
+              <h2>근처 빵 집을 찾아보세요.</h2>
+              <h3>따끈따끈한 빵들이 기다리고 있습니다.</h3>
+              <a href="#" id="carouselLink">빵 집 찾기</a>
+            </div>
+          </div>
+          <div class="carousel-item item2">
+            <div className="helper"></div><div className="intro">
+              <h2>어떤 빵을 드시고 싶으신가요??</h2>
+              <h3>고객님이 원하시는 빵을 한눈에!</h3>
+              <a href="#" id="carouselLink">빵 찾기</a>
+            </div>
+          </div>
+          <div className="carousel-item item3">
+            <div className="helper"></div><div className="intro">
+              <h2>혹시 선택하기 힘드신가요??</h2>
+              <h3>고객님께 빵 집을 추천해드립니다.</h3>
+              <a href="#" id="carouselLink">빵 집 추천</a>
+            </div>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+  
+  
+  
     </body>
   );
 }
 
-export default navbar;
+export default main;
