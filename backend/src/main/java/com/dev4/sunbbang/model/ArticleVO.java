117a4bdac7fa8c65b55e98f25e184ced3eec79bd
sungminju7488 <ssmj94@hanmail.net>
2021-06-08 01:52:54 +0900

    Mybatis로 articlelist구현
    ArticleVO.java
    ArticleController.java
    ArticleService.java
    ArticleServiceImpl.java
    ArticleDAO.java
    내용수정

--- backend/src/main/java/com/dev4/sunbbang/model/ArticleVO.java
+++ backend/src/main/java/com/dev4/sunbbang/model/ArticleVO.java
@@ -1,55 +1,61 @@
 package com.dev4.sunbbang.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-
-import org.springframework.validation.annotation.Validated;
-
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-@Validated
-@Data
-@Getter@Setter
-@SequenceGenerator(name = "articleSeq",sequenceName = "no_seq",initialValue = 1 , allocationSize = 1)
-@Entity
-@Table(name ="ARTICLE")
 public class ArticleVO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator ="no_seq")
-	@Column(name="ARTICLESEQ",nullable=false, unique = true)
 	private int articleSeq;
-	
-	@Column(name="TITLE",nullable=false)
 	private String title;
-	
-	@Column(name="WRITERSEQ",nullable=false)
 	private int writerSeq;
-	
-	@Column(name="WRITERNICKNAME",nullable=false)
 	private String writerNickname;
-	
-	@Column(name="REGDATE",nullable=false)
 	private String regDate;
-	
-	@Column(name="SCORE")
 	private int score;
-	
-	@Column(name="REPORT",nullable=false)
 	private String report;
-	
-	@ManyToOne
-	@JoinColumn(name ="MEMBERSEQ")
-	@Column(name="BAKERYSEQ",nullable=false)
 	private int bakerySeq;
-	
+	public int getArticleSeq() {
+		return articleSeq;
+	}
+	public void setArticleSeq(int articleSeq) {
+		this.articleSeq = articleSeq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getWriterSeq() {
+		return writerSeq;
+	}
+	public void setWriterSeq(int writerSeq) {
+		this.writerSeq = writerSeq;
+	}
+	public String getWriterNickname() {
+		return writerNickname;
+	}
+	public void setWriterNickname(String writerNickname) {
+		this.writerNickname = writerNickname;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getReport() {
+		return report;
+	}
+	public void setReport(String report) {
+		this.report = report;
+	}
+	public int getBakerySeq() {
+		return bakerySeq;
+	}
+	public void setBakerySeq(int bakerySeq) {
+		this.bakerySeq = bakerySeq;
+	}
 }
---
+++ backend/src/main/webapp/WEB-INF/tags/isLogin.tag
@@ -0,0 +1,11 @@
+<%@ tag body-content="scriptless" pageEncoding="UTF-8"%>
+<%@ tag trimDirectiveWhitespaces="true" %>
+<%
+	HttpSession httpSession = request.getSession(false);
+	if(httpSession != null && httpSession.getAttribute("member") != null)
+	{
+%>
+	<jsp:doBody/>
+<% 	
+	}
+%>
