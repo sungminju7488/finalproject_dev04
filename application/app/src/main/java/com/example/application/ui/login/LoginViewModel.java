009b628eca36a76cd52888a51858b44fb96fa618
SEOKJY\sky-s <jiyunseok45@gmail.com>
2021-06-08 21:26:26 +0900

    안드로이드 로그인뷰모델  커밋

--- application/app/src/main/java/com/example/application/ui/login/LoginViewModel.java
+++ application/app/src/main/java/com/example/application/ui/login/LoginViewModel.java
@@ -2,35 +2,28 @@ package com.example.application.ui.login;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
-import com.example.application.model.LoginInfo;
 import com.example.application.model.MemberVO;
-import com.example.application.repository.MemberRepository;
+import com.example.application.repository.AppRepository;
 
 import retrofit2.Callback;
 
 public class LoginViewModel extends ViewModel {
-    private final MemberRepository repository;
+    private final AppRepository repository;
     public MutableLiveData<String> id;
     public MutableLiveData<String> password;
 
-    public LoginViewModel(MemberRepository repository) {
+    public LoginViewModel(AppRepository repository) {
         this.repository = repository;
         id = new MutableLiveData<>();
         password = new MutableLiveData<>();
     }
 
-    /**
-     * 로그인 요청
-     * @param callback 요청에 대한 응답 콜백
-     */
+
     void login(Callback<MemberVO> callback) {
-        repository.loginMember(new LoginInfo(id.getValue(), password.getValue()), callback);
+        repository.loginMember(new MemberVO(id.getValue(), password.getValue()), callback);
     }
 
-    /**
-     * 아이디 비밀번호가 비어있진 않은지 검사
-     */
+
     boolean validateLoginInfo() {
         return id.getValue() != null && password.getValue() != null;
     }
