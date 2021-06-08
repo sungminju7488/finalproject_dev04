78112f2df00a9b6a9a1cd1ec83a72c86ae52b0d4
NaudhizFehu <naudhizfehu@gmail.com>
2021-06-08 12:23:39 +0900

    로그인 작업중

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/login.css";
 
 function Login() {
-  return <h1> 로그인 페이지 패치중 ^^</h1>;
+  const [memberId, setMemberId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log("id : " + memberId + " | pw : " + password);
+  };
+
+  return (
+    <div id="content">
+      <div id="header">
+        <a href="/" target="_self" title="선빵 회원가입 페이지 보러가기">
+          <span id="sunbbang">선 빵</span>
+        </a>
+      </div>
+      <form onSubmit={handleLogin}>
+        <span className="box id">
+          <input
+            type="text"
+            id="id"
+            className="var"
+            maxLength="20"
+            placeholder="아이디"
+            onChange={(e) => {
+              setMemberId(e.target.value);
+            }}
+          ></input>
+        </span>
+        <span className="box password">
+          <input
+            type="password"
+            id="password"
+            className="var"
+            maxLength="20"
+            placeholder="비밀번호"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+        </span>
+        <div id="btn_area">
+          <button type="submit" id="login_btn">
+            <span>로 그 인</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/component/Main.js
+++ frontend/src/component/Main.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
+import "../css/carousel.css";
 
-function navbar() {
+function main() {
   return (
-    <div>
+    <body>
       <nav className="navbar navbar-expand-lg navbar-light bg-#f0e8d9">
-        <Link to="/" className="navbar-brand" id="BrandName">
+        <a className="navbar-brand" href="#" ID="BrandName">
           선 빵
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,7 +24,7 @@ function navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/service/backeryMap" className="nav-link">
+              <Link to="#" className="nav-link">
                 빵 집 찾기
               </Link>
             </li>
@@ -46,20 +47,91 @@ function navbar() {
 
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/member/join1" className="nav-link">
+              <Link to="/member/joinpage" className="nav-link">
                 회원가입
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/member/login1" className="nav-link">
+              <Link to="/member/loginpage" className="nav-link">
                 로그인
               </Link>
             </li>
           </ul>
         </div>
       </nav>
-    </div>
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item item1 active">
+            <div className="helper"></div>
+            <div className="intro">
+              <h2>근처 빵 집을 찾아보세요.</h2>
+              <h3>따끈따끈한 빵들이 기다리고 있습니다.</h3>
+              <a href="#" id="carouselLink">
+                빵 집 찾기
+              </a>
+            </div>
+          </div>
+          <div class="carousel-item item2">
+            <div className="helper"></div>
+            <div className="intro">
+              <h2>어떤 빵을 드시고 싶으신가요??</h2>
+              <h3>고객님이 원하시는 빵을 한눈에!</h3>
+              <a href="#" id="carouselLink">
+                빵 찾기
+              </a>
+            </div>
+          </div>
+          <div className="carousel-item item3">
+            <div className="helper"></div>
+            <div className="intro">
+              <h2>혹시 선택하기 힘드신가요??</h2>
+              <h3>고객님께 빵 집을 추천해드립니다.</h3>
+              <a href="#" id="carouselLink">
+                빵 집 추천
+              </a>
+            </div>
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </body>
   );
 }
 
-export default navbar;
+export default main;
