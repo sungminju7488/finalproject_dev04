813b9d06962b72544389e88320089439b6aca1e0
NaudhizFehu <naudhizfehu@gmail.com>
2021-06-09 23:27:40 +0900

    - 필요하지 않은 dependency 제거

--- backend/src/main/java/com/dev4/sunbbang/util/JwtManager.java
+++ backend/src/main/java/com/dev4/sunbbang/util/JwtManager.java
@@ -7,7 +7,6 @@ import java.util.Date;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import com.dev4.sunbbang.model.AuthVO;
 
