cdeaca35c9dcd4d957951fd5fc0949d57db0b143
signatureh <signature-h@hanmail.net>
2021-06-16 11:19:57 +0900

    게시판 기능단 백엔드 코드 디버깅 및 코드 최적화

--- backend/src/main/java/com/dev4/sunbbang/article/ArticleService.java
+++ backend/src/main/java/com/dev4/sunbbang/article/ArticleService.java
@@ -16,6 +16,7 @@ import com.dev4.sunbbang.model.MemberVO;
 import com.dev4.sunbbang.model.PageVO;
 import com.dev4.sunbbang.repository.ArticleRepository;
 import com.dev4.sunbbang.repository.BakeryRepository;
+import com.dev4.sunbbang.util.Define;
 
 @Transactional
 @Service
@@ -23,59 +24,74 @@ public class ArticleService {
 
 	@Autowired
 	ArticleRepository articleRepository;
+
 	@Autowired
 	BakeryRepository bakeryRepository;
 
-	public Page<ArticleVO> articleList(BakeryVO bakeryVO,PageVO pageVO) {
-		
-		return articleRepository.findList(pageVO.getKeyword(),bakeryVO, PageRequest.of(pageVO.getPageNo(), pageVO.getPageSize()));
+	public Page<ArticleVO> articleList(BakeryVO bakeryVO, PageVO pageVO) {
+		return articleRepository.findByBakeryVO(bakeryVO, PageRequest.of(pageVO.getPageNo(), pageVO.getPageSize()))
+				.get();
 	}
-	
-	public void writeArticle(MemberVO memberVO,ArticleVO articleVO,BakeryVO bakeryVO,MultipartFile image,String imageName) throws IOException {
-		articleVO.setArticleSeq(memberVO.getMemberSeq());
-		articleVO.setWriterNickname(memberVO.getNickName());
+
+	public void writeArticle(MemberVO memberVO, ArticleVO articleVO, BakeryVO bakeryVO, MultipartFile image,
+			String imageName) throws IOException {
 		bakeryVO = bakeryRepository.findById(bakeryVO.getCopRegNum()).get();
 		articleVO.setBakeryVO(bakeryVO);
-		
-		int articleSeq;
-		if(articleRepository.findAll().size()==0) {
-			articleSeq = 1;	
-		}else {
-			articleSeq = articleRepository.getArticleSeq().get()+1;
+		articleVO.setWriterSeq(memberVO.getMemberSeq());
+		articleVO.setWriterNickname(memberVO.getNickName());
+		articleRepository.save(articleVO);
+		if (!image.isEmpty()) {
+			int articleSeq = articleRepository.getArticleSeq().get();
+			String fileName = articleSeq + "." + imageName.substring(imageName.lastIndexOf(".") + 1);
+			String path = Define.IMAGE_LOAD_PATH + "article/" + fileName;
+			String savePath = Define.IMAGE_SAVE_PATH + "article/" + fileName;
+			image.transferTo(new File(savePath));
+			articleVO.setArticlePath(path);
+			articleVO.setArticleSavePath(savePath);
 		}
-		String fileName = articleSeq + "." + imageName.substring(imageName.lastIndexOf(".") + 1);
-		image.transferTo(new File("C://images/article/" + fileName));
-		String path = "http://localhost:8080/images/article/" + fileName;
-		articleVO.setArticlePath(path);
 		articleRepository.save(articleVO);
 	}
-	public ArticleVO readArticle(ArticleVO vo) {
-		return articleRepository.findByArticleSeq(vo.getArticleSeq()).get();
+
+	public ArticleVO readArticle(ArticleVO articleVO) {
+		return articleRepository.findByArticleSeq(articleVO.getArticleSeq()).get();
 	}
-	public void modifyArticle(ArticleVO vo) {
-		articleRepository.save(vo);
+
+	public void modifyArticle(ArticleVO articleVO, BakeryVO bakeryVO, MultipartFile image, String imageName)
+			throws IOException {
+		if (image != null && !image.isEmpty()) {
+			String fileName = articleVO.getArticleSeq() + "." + imageName.substring(imageName.lastIndexOf(".") + 1);
+			String path = Define.IMAGE_LOAD_PATH + "article/" + fileName;
+			String savePath = Define.IMAGE_SAVE_PATH + "article/" + fileName;
+			image.transferTo(new File(savePath));
+			articleVO.setArticlePath(path);
+			articleVO.setArticleSavePath(savePath);
+		}
+		articleVO.setBakeryVO(bakeryRepository.findById(bakeryVO.getCopRegNum()).get());
+		articleRepository.save(articleVO);
 	}
-	public void deleteArticle(ArticleVO vo) {
-		articleRepository.delete(vo);
-		if (vo.getArticlePath() != null) {
-			File file = new File(vo.getArticlePath());
+
+	public void deleteArticle(ArticleVO articleVO) {
+		articleRepository.delete(articleVO);
+		if (articleVO.getArticleSavePath() != null) {
+			File file = new File(articleVO.getArticleSavePath());
 			file.delete();
 		}
 	}
-	public void writeReply(ArticleVO vo) {
-		articleRepository.save(vo);
+
+	public void writeReply(ArticleVO articleVO) {
+		articleRepository.save(articleVO);
 	}
-	public void modifyReply(ArticleVO vo) {
-		articleRepository.save(vo);
+
+	public void deleteReply(ArticleVO articleVO) {
+		articleVO.setReply(null);
+		articleRepository.save(articleVO);
 	}
-	public void deleteReply(ArticleVO vo) {
-		articleRepository.delete(vo);
-	}
-	public void reportArticle(ArticleVO vo) {
-		int totalReport=Integer.parseInt(vo.getReport());
-	if(totalReport<5)
-		totalReport=totalReport+1;
-	else 
-		articleRepository.reportArticle(vo);
-	}
+
+//	public void reportArticle(ArticleVO vo) {
+//		int totalReport = Integer.parseInt(vo.getReport());
+//		if (totalReport < 5)
+//			totalReport = totalReport + 1;
+//		else
+//			articleRepository.reportArticle(vo);
+//	}
 }
