d945c71b96149d61e82ea7ef4c14e31bc84a416f
NaudhizFehu <naudhizfehu@gmail.com>
2021-06-23 14:21:10 +0900

    비밀번호 찾기 구현완료

--- frontend/src/component/FindPasswordResult.js
+++ frontend/src/component/FindPasswordResult.js
@@ -1,11 +1,28 @@
+import axios from "axios";
 import { useState } from "react";
 
 function FindPasswordResult(props) {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const moveLoginHandler = () => {
-    window.location.replace("/member/loginpage");
+  const changePasswordHandler = () => {
+    if (password === "" || confirmPassword === "")
+      return alert("새 비밀번호와 재확인 비밀번호를 모두 입력해주세요");
+    else if (password !== confirmPassword)
+      return alert("새 비밀번호와 재확인 비밀번호가 다릅니다.");
+
+    const memberSeq = props.location.state.memberSeq;
+    axios
+      .post("/changePassword", { memberSeq, password })
+      .then((res) => {
+        if (res.data === true) {
+          alert("비밀번호가 변경되었습니다.");
+          window.location.replace("/member/loginpage");
+        } else {
+          alert("비밀번호 변경에 실패하였습니다.");
+        }
+      })
+      .catch((err) => alert(err.response.data.msg));
   };
 
   return (
@@ -16,41 +33,43 @@ function FindPasswordResult(props) {
           <span id="sunbbang">비밀번호 변경</span>
         </a>
       </div>
-      {/* pw 1차 부분 */}
-      <h3 className="join_title">
-        <label>새 비밀번호</label>
-      </h3>
-      <span className="box var_pass">
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          className="var"
-          maxLength="20"
-        ></input>
-      </span>
-      {/* pw 2차 부분 */}
-      <h3 className="join_title">
-        <label>비밀번호 재확인</label>
-      </h3>
-      <span className="box var_pass_check">
-        <input
-          type="password"
-          id="confirmPassword"
-          onChange={(e) => {
-            setConfirmPassword(e.target.value);
-          }}
-          className="var"
-          maxLength="20"
-        ></input>
-      </span>
-      <div id="btn_area">
-        <button type="button" id="login_btn" onClick={moveLoginHandler}>
-          <span>비밀번호 변경</span>
-        </button>
-      </div>
+      <form onSubmit={changePasswordHandler}>
+        {/* pw 1차 부분 */}
+        <h3 className="join_title">
+          <label>새 비밀번호</label>
+        </h3>
+        <span className="box var_pass">
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="var"
+            maxLength="20"
+          ></input>
+        </span>
+        {/* pw 2차 부분 */}
+        <h3 className="join_title">
+          <label>비밀번호 재확인</label>
+        </h3>
+        <span className="box var_pass_check">
+          <input
+            type="password"
+            id="confirmPassword"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+            className="var"
+            maxLength="20"
+          ></input>
+        </span>
+        <div id="btn_area">
+          <button type="submit" id="login_btn">
+            <span>비밀번호 변경</span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
